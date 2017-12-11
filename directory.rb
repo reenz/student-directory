c9fd557423c5ad30a778773c78a4b89de3640bf9
Reena <reena.sanjay.sharma@gmail.com>
2017-12-11 20:43:18 +0000

    Added hash to print student and cohort month

--- directory.rb
+++ directory.rb
@@ -1,7 +1,17 @@
 
-students = ["Dr. Hannibal Lecter","Darth Vader","Nurse Ratched","Michael Corleone",
-  "Alex DeLarge","The Wicked Witch of the West","Terminator",
-  "Freddy Krueger","The Joker","Joffrey Baratheon","Norman Bates"]
+students = [
+  {name: "Dr. Hannibal Lecter",cohort: :novmember},
+  {name: "Darth Vader",cohort: :novmember},
+  {name: "Nurse Ratched",cohort: :novmember},
+  {name: "Michael Corleone",cohort: :novmember},
+  {name: "Alex DeLarge",cohort: :novmember},
+  {name: "The Wicked Witch of the West",cohort: :novmember},
+  {name: "Terminator",cohort: :novmember},
+  {name: "Freddy Krueger",cohort: :novmember},
+  {name: "The Joker",cohort: :novmember},
+  {name: "Joffrey Baratheon",cohort: :novmember},
+  {name: "Norman Bates",cohort: :novmember}
+]
 
 def print_header
 puts "The students of Villains Academy"
@@ -10,7 +20,7 @@ end
 
 def print(names)
 names.each do |name|
-  puts name
+  puts "#{name[:name]} (#{name[:cohort]} cohort)"
 end
 end
 
