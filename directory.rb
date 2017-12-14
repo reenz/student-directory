b73082b4ddfcf5a3a86de92eacd4b7128f7d75ea
Reena <reena.sanjay.sharma@gmail.com>
2017-12-14 17:10:18 +0000

    Refactored program

--- directory.rb
+++ directory.rb
@@ -1,16 +1,49 @@
-
+@students = []
 def input_students
   puts "Please enter the names of students"
   puts "To finish , just hit return twice"
-  students = []
+  #students = []
   name = gets.chomp
 
   while !name.empty? do
-    students << {name: name , cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name , cohort: :november}
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
   end
-  students
+  #students
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list(@students)
+  print_footer(@students)
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I dont't know what you meant, try again"
+  end
+end
+
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
 end
 
 def print_header
@@ -18,7 +51,7 @@ def print_header
   puts "-------------"
 end
 
-def print(names)
+def print_students_list(names)
   i = 0
   while i < names.count do
     name = names[i]
@@ -31,7 +64,4 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+interactive_menu
